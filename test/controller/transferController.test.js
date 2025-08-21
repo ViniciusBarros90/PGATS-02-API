6e00912dbdc43f33c59d930b426fc68274a96af1
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2025-08-21 19:21:17 -0300

    diconando um novo teste de erro na transferencia com mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -7,6 +7,9 @@ const { expect } = require('chai');
 const app = require('../../app');
 const { describe } = require('mocha');
 
+//Mock
+const transferService = require('../../service/transferService');
+
 //Testes
 describe('Transfer Controller', () => {
     describe('POST /api/transfers', () => {
@@ -23,7 +26,27 @@ describe('Transfer Controller', () => {
             expect(response.body).to.have.property('error', 'Sender or recipient not found');
         });
 
-    })
+        it('Usando Mocks: Quando  informo remetente de destinatário inexistente recebo 400', async () => {
+            //Mocar apenas a função externa transfer do Service
+            const transferServiceMock = sinon.stub(transferService, 'transfer');
+            transferServiceMock.throws(new Error('Sender or recipient not found'));
+            
+            const response = await request(app)
+                .post('/api/transfers')
+                .send({
+                    from: "vinicius",
+                    to: "julio",
+                    amount: 100
+                });
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property('error', 'Sender or recipient not found');
+
+            //Reseta o Mock
+            sinon.restore();
+        });
+
+    });
 
     describe('GET /api/transfers', () => {
         //it aqui
