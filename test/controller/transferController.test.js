da20addf116e050a17a9cc5f6de61c183d373c5d
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2025-08-22 21:24:37 -0300

    Subir a execução de teste de API extrenal já com o arquivo yaml alterado

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -46,6 +46,33 @@ describe('Transfer Controller', () => {
             sinon.restore();
         });
 
+        it('Usando Mocks: Quando  informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+            //Mocar apenas a função externa transfer do Service
+            const transferServiceMock = sinon.stub(transferService, 'transfer');
+            transferServiceMock.returns({
+                from: "vinicius",
+                to: "julio",
+                amount: 100,
+                date: new Date().toISOString()
+            });
+            
+            const response = await request(app)
+                .post('/api/transfers')
+                .send({
+                    from: "vinicius",
+                    to: "julio",
+                    amount: 100
+                });
+
+            expect(response.status).to.equal(201);
+            expect(response.body).to.have.property('from', 'vinicius');
+            expect(response.body).to.have.property('to', 'julio');
+            expect(response.body).to.have.property('amount', 100);
+
+            //Reseta o Mock
+            sinon.restore();
+        });
+
     });
 
     describe('GET /api/transfers', () => {
