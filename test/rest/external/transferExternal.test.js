5dbdb554ef71e4f939f4484a85c123db6526f405
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2025-09-16 17:30:14 -0300

    Incluindo o fixture de erro na API rest

--- test/rest/external/transferExternal.test.js
+++ test/rest/external/transferExternal.test.js
@@ -8,32 +8,33 @@ require('dotenv').config();
 //Testes
 describe('API REST Transfer External', () => {
   describe('POST /api/transfers', () => {
-    it('Quando informo remetente de destinatário inexistente em uma API external, recebo 400', async () => {
+    before(async () => {
+      const postLogin = require('../fixture/request/login/postLogin.json');
 
     // 1 - Capturar o token
       const loginResponse = await request(process.env.BASE_URL_REST)
         .post('/api/users/login')
-        .send({
-          username: 'vinicius',
-          password: 'julio'
-        })
-      const token = loginResponse.body.token;
+        .send(postLogin);
 
-     
+      token = loginResponse.body.token;
+    });
+        
+
     // 2 - Realizar a transferência
-      const response = await request(process.env.BASE_URL_REST)
-        .post('/api/transfers')
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          from: "vinicius",
-          to: "maria",
-          amount: 200
-          });
+    const testesDeErrosDeNegocios = require('../fixture/transfers/postTransfersWithError.json');
+    testesDeErrosDeNegocios.forEach((teste) => {
+      it(`Testando a regra relacionada a ${teste.nomeDoTeste}`, async () => { 
+        const response = await request(process.env.BASE_URL_REST)
+          .post('/api/transfers')
+          .set('Authorization', `Bearer ${token}`)
+          .send(teste.postTransfers);
 
-      expect(response.status).to.equal(400);
-      expect(response.body).to.have.property('error', 'Sender or recipient not found');
+        expect(response.status).to.equal(teste.statusCode);
+        expect(response.body).to.have.property('error', teste.mensagemEsperada);
       });
 
-   });    
+    });    
    
+  });
+
 });
