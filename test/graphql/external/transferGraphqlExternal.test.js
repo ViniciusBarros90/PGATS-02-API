a14d34b59fadeb5ef73e863a3a9d1d6f9fa391d9
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2025-09-16 12:38:17 -0300

    Refatorando o código transferGraphqlEternal e criando o Fixture Json de erro

--- test/graphql/external/transferGraphqlExternal.test.js
+++ test/graphql/external/transferGraphqlExternal.test.js
@@ -30,18 +30,6 @@ describe('API GRAPHQL Transfer External', () => {
         expect(resposta.body).to.eql(respostaEsperada);
     });
 
-    it('Sem saldo disponível para transferência', async () => {
-        transfer.variables.amount = 100000000; 
-
-        const resposta = await request(process.env.BASE_URL_GRAPHQL)
-            .post('')
-            .set('Authorization', 'Bearer ' + token)
-            .send(transfer);
-
-        expect(resposta.body).to.have.property('errors');
-        expect(resposta.body.errors[0].message).to.equal('Insufficient balance');
-    });
-
     it('Token de autenticação não informado', async () => {
         const resposta = await request(process.env.BASE_URL_GRAPHQL)
             .post('')
@@ -49,5 +37,19 @@ describe('API GRAPHQL Transfer External', () => {
 
         expect(resposta.body).to.have.property('errors');
         expect(resposta.body.errors[0].message).to.equal('Not authenticated');
+    });
+
+    const testesDeErroDeNegocios = require('../fixture/request/transfer/createTransferWithError.json');
+    testesDeErroDeNegocios.forEach((teste) => {
+        it('Sem saldo disponível para transferência', async () => {
+            const resposta = await request(process.env.BASE_URL_GRAPHQL)
+                .post('')
+                .set('Authorization', 'Bearer ' + token)
+                .send(teste.transferJson);
+
+        expect(resposta.body).to.have.property('errors');
+        expect(resposta.body.errors[0].message).to.equal(teste.mensagemEsperada);
+      });
+   
     });
 });
